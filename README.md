fcdda8f6eeabcbb2db26cae646ea1f97fd7c26da
Basti <basti@nanocosmos.de>
2021-08-30 07:58:42 +0200

    make it work somehow

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # logstash-operator
 
-WIP
+Disclaimer: Kind of works, but still missing a lot. Probably not following any best practice, and will certainly fail in many edge cases.
 
+Known issues (for now):
+- Logstash autoreload seems not to work
+- Service not updating correctly when changing output
+- Not found exception thrown when deleting stuff (cause update triggered on pipeline configmap after child was deleted by kubernetes)
--- operator/logstash-operator.py
+++ operator/logstash-operator.py
@@ -23,8 +23,9 @@ def create_configmap(name,namespace):
     )
     return obj
 
-@kopf.on.create('logstash-deployment')
-def create_statefulset_fn(spec, name, namespace, logger, **kwargs):
+@kopf.on.create('logstash-deployment',param={'type':'deployment','action':'create'})
+@kopf.on.update('logstash-deployment',param={'type':'deployment','action':'update'})
+def create_statefulset_fn(param, spec, name, namespace, logger, **kwargs):
     logger.info(f"Creating: {name}")
 
     replicas = spec.get('replicas')
@@ -34,65 +35,126 @@ def create_statefulset_fn(spec, name, namespace, logger, **kwargs):
     logstashconfig = spec.get('config')
     image = spec.get('image')
 
+    configconfigmapname = name+"-logstash-operator-config"
+    pipelinesconfigmapname = name+"-logstash-operator-pipelines"
+
     templateLoader = jinja2.FileSystemLoader(searchpath="./templates/")
     templateEnv = jinja2.Environment(loader=templateLoader)
 
+    ## render configmap for logstash config
     TEMPLATE_FILE = "configmap-config.yaml.j2"
     template = templateEnv.get_template(TEMPLATE_FILE)
-    text = template.render(name=name,config=logstashconfig)
-    data = yaml.safe_load(text)
-    kopf.adopt(data)
-    api = kubernetes.client.CoreV1Api()
-    obj = api.create_namespaced_config_map(
-        namespace=namespace,
-        body=data,
-    )
+    text = template.render(name=configconfigmapname,config=logstashconfig)
+    configdata = yaml.safe_load(text)
 
+    ## Render configmap for logstash pipelines config locations
     TEMPLATE_FILE = "configmap-pipelines.yaml.j2"
     template = templateEnv.get_template(TEMPLATE_FILE)
-    text = template.render(name=name,pipelines=pipelines)
-    data = yaml.safe_load(text)
-    kopf.adopt(data)
-    api = kubernetes.client.CoreV1Api()
-    obj = api.create_namespaced_config_map(
-        namespace=namespace,
-        body=data,
-    )
+    text = template.render(name=pipelinesconfigmapname,pipelines=pipelines)
+    pipelinesdata = yaml.safe_load(text)
 
+    ## Get all inputs for this statefulset and create service for each
+    services={}
+    ports={}
+    customobjectsapi = kubernetes.client.CustomObjectsApi()
+    customobjectsapi_response = customobjectsapi.list_namespaced_custom_object("logstash-operator.qalo.de","v1",namespace,"logstash-inputs", pretty="true", label_selector=spec.get('selector'))
+    TEMPLATE_FILE = "service.yaml.j2"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    for item in customobjectsapi_response['items']:
+        ports[item['metadata']['name']]=item['spec']['port']
+        servicename=name+"-"+item['metadata']['name']
+        text = template.render(name=servicename,app=name,port=item['spec']['port'],portname=item['metadata']['name'])
+        servicedata = yaml.safe_load(text)
+        services[servicename]=servicedata
 
+    ## Render statefulset manifest
     TEMPLATE_FILE = "statefulset.yaml.j2"
     template = templateEnv.get_template(TEMPLATE_FILE)
-    text = template.render(name=name, replicas=replicas, pipelines=pipelines, image=image)
-    data = yaml.safe_load(text)
-    kopf.adopt(data)
-    api = kubernetes.client.AppsV1Api()
-    obj = api.create_namespaced_stateful_set(
-        namespace=namespace,
-        body=data,
-    )
-    logger.info(f"Statefulset child is created: {obj.metadata.name}")
-    return {'objname': obj.metadata.name}
+    text = template.render(name=name, replicas=replicas, pipelines=pipelines, image=image, ports=ports)
+    statefulsetdata = yaml.safe_load(text)
 
-@kopf.on.update('logstash-deployment')
-def update_statefulset_fn(spec, status, namespace, logger, **kwargs):
+    # Create
+    if param['action'] == 'create':
+        ## Create configmap for logstash config
+        kopf.adopt(configdata)
+        api = kubernetes.client.CoreV1Api()
+        obj = api.create_namespaced_config_map(
+            namespace=namespace,
+            body=configdata,
+        )
+        logger.info(f"Created configmap: {obj.metadata.name}")
 
-    replicas = spec.get('replicas')
-    if not replicas:
-        raise kopf.PermanentError(f"Replicas must be set. Got {replicas!r}.")
+        ## Create configmap for logstash pipelines config locations
+        kopf.adopt(pipelinesdata)
+        api = kubernetes.client.CoreV1Api()
+        obj = api.create_namespaced_config_map(
+            namespace=namespace,
+            body=pipelinesdata,
+        )
+        logger.info(f"Created configmap: {obj.metadata.name}")
 
-    statefulset_name = status['create_statefulset_fn']['objname']
-    statefulset_patch = {'spec': {'replicas': replicas}}
+        ## Crete Statefulset
+        kopf.adopt(statefulsetdata)
+        api = kubernetes.client.AppsV1Api()
+        obj = api.create_namespaced_stateful_set(
+            namespace=namespace,
+            body=statefulsetdata,
+        )
+        logger.info(f"Created Statefulset: {obj.metadata.name}")
 
-    logger.info(f"Updating: {statefulset_name}")
+
+    ## Update 
+    if param['action'] == 'update':
+
+        ## Update configmap for logstash config
+        kopf.adopt(configdata)
+        api = kubernetes.client.CoreV1Api()
+        obj = api.patch_namespaced_config_map(
+            namespace=namespace,
+            name = configconfigmapname,
+            body=configdata,
+        )
     
-    api = kubernetes.client.AppsV1Api()
-    obj = api.patch_namespaced_stateful_set(
-        namespace=namespace,
-        name=statefulset_name,
-        body=statefulset_patch,
-    )
+        ## Update configmap for logstash pipelines config locations
+        kopf.adopt(pipelinesdata)
+        api = kubernetes.client.CoreV1Api()
+        obj = api.patch_namespaced_config_map(
+            namespace=namespace,
+            name = pipelinesconfigmapname,
+            body=pipelinesdata,
+        )
+        logger.info(f"Updated pipeline configmap: {obj.metadata.name}")
 
-    logger.info(f"Statefulset child is updated: {obj.metadata.name}")
+        # Update statefulset
+        api = kubernetes.client.AppsV1Api()
+        obj = api.patch_namespaced_stateful_set(
+            namespace=namespace,
+            name=name,
+            body=statefulsetdata,
+        )
+        logger.info(f"Updated Statefulset: {obj.metadata.name}")
+
+    # Update or create services
+    for servicename,servicedata in services.items():
+        api = kubernetes.client.CoreV1Api()
+        try: 
+            api_response = api.read_namespaced_service(servicename, namespace, pretty="true")
+        except ApiException as e:
+            if (e.status == 404):
+                kopf.adopt(servicedata)
+                obj = api.create_namespaced_service(
+                    namespace=namespace,
+                    body=servicedata,
+                )
+                logger.info(f"Created service: {obj.metadata.name}")
+        else:
+            print(servicedata)
+            obj = api.replace_namespaced_service (
+                namespace=namespace,
+                name = name+"-"+item['metadata']['name'],
+                body=servicedata,
+            )
+            logger.info(f"Updated service: {obj.metadata.name}")
 
 
 @kopf.on.create('logstash-pipeline',param={'type':'pipeline','action':'create'})
@@ -178,3 +240,14 @@ def pipelineelement_fn(param,spec, name, namespace, logger, **kwargs):
             name=item['metadata']['name'],
             body=patch,
         )
+    if param['type'] == 'input':
+        customobjectsapi_response = customobjectsapi.list_namespaced_custom_object("logstash-operator.qalo.de","v1",namespace,"logstash-deployments", pretty="true")
+        for item in customobjectsapi_response['items']:
+            patchversion = int(item['metadata'].get('annotations',{}).get('logstash-operator.qalo.de/patchversion',"0"))+1
+            patch = {'metadata':{'annotations':{'logstash-operator.qalo.de/patchversion' : str(patchversion)}}}
+            logger.info(f"Trigger update on deployment {item['metadata']['name']}, patchversion {patchversion}")
+
+            obj = customobjectsapi.patch_namespaced_custom_object("logstash-operator.qalo.de","v1",namespace,"logstash-deployments",
+                name=item['metadata']['name'],
+                body=patch,
+            )
